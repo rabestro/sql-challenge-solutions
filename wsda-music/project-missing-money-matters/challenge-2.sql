ac3c44c755f56f6e518f19a72fc9d6ac55413310
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-07-06 18:04:19 +0300

    Add challenge 3 queries

--- wsda-music/project-missing-money-matters/challenge-2.sql
+++ wsda-music/project-missing-money-matters/challenge-2.sql
@@ -1,7 +1,7 @@
 -- CHALLENGE 2
 -- More targeted questions that query tables containing data about customers and employees
 
-CREATE TEMPORARY TABLE purchases_2011_2012 AS
+CREATE TEMPORARY TABLE IF NOT EXISTS purchases_2011_2012 AS
 SELECT
     *
 FROM
@@ -12,27 +12,30 @@ WHERE
 -- 1. Get a list of customers who made purchases between 2011 and 2012.
 
 SELECT DISTINCT
-    customerid
+    customerid,
+    firstname || ' ' || lastname AS customer_name,
+    total
 FROM
-    purchases_2011_2012;
+    purchases_2011_2012
+    INNER JOIN customer USING (customerid);
 
 -- 2. Get a list of customers, sales reps, and total transaction amounts for each customer between 2011 and 2012.
 
 SELECT
     c.firstname || ' ' || c.lastname AS customer_name,
     e.firstname || ' ' || e.lastname AS sales_rep,
-    ROUND(SUM(total), 2)             AS total_revenue
+    total
 FROM
     purchases_2011_2012
     INNER JOIN customer c USING (customerid)
     INNER JOIN employee e ON c.supportrepid = e.employeeid
-GROUP BY
-    customerid;
+ORDER BY
+    total DESC;
 
 -- 3. How many transactions are above the average transaction amount during the same time period?
 
 SELECT
-    COUNT(*)
+    COUNT(*) AS above_avg_transactions
 FROM
     purchases_2011_2012
 WHERE
@@ -60,3 +63,11 @@ GROUP BY
 ORDER BY
     year;
 
+-- Other solutions
+SELECT
+    STRFTIME('%Y', invoicedate) AS [Year],
+    ROUND(AVG(total), 2)        AS [Avg Transaction Amount]
+FROM
+    invoice
+GROUP BY
+    STRFTIME('%Y', invoicedate)
